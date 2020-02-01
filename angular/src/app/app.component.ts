ecde9fa214efca5f0a8db59f6b57d6c87c1c71ff
tubaleviao <alvaro.prata.filho@gmail.com>
2020-02-01 10:45:34 -0600

    basic angular code

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+
+  <a [routerLink]="['']">Home</a> | 
+  <a [routerLink]="['login']">Login</a>
+  <router-outlet></router-outlet>
+`
+})
+export class AppComponent {
+  title = 'app works!';
+}
---
+++ angular/src/app/components/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home',
+  template: `<p>home works!</p>`
+})
+export class HomeComponent {
+
+}
