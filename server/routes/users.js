c1779eb3d272cf0f68e02c871cfd61d8e4c94ca8
Yosief Teklemariam <yosiefnt@gmail.com>
2020-02-01 15:08:52 -0600

    users route updated

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,15 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json({msg:"something"})
-  // req.db.find({},  { '_id': 0, 'username': 1, 'email': 1 } )
-  //       .toArray((err, docs) => {
-  //           res.status(200).json(docs);
-  //       });
+  //res.json({msg:"something"})
+  req.db.find({},  { '_id': 0, 'username': 1, 'email': 1 } )
+        .toArray((err, documents) => {
+            res.status(200).json(documents);
+        });
 });
 
 // /* GET user, find one. */
-router.get("/findOne/:username", (req, res) => {
+router.get("/:username", (req, res) => {
   const query = { username: req.params.username };
   req.db.collection('users').find(query).toArray((error, document) => {
     if (error) throw error;
@@ -19,20 +19,18 @@ router.get("/findOne/:username", (req, res) => {
   });
 });
 
-
-
 /* POST creating users */
 router.post('/create', function(req, res, next){
-  req.db.insert({ 'username': req.body.username, 'password': req.body.password, 'email': req.body.email}, (err, data) => {
-      res.json({ userId:' _id' })
-  });
+  //req.db.insert({ 'username': req.body.username, 'password': req.body.password, 'email': req.body.email}, (err, data) => {
+      res.json({ userId:'_id' })
+  //});
 })
 
-// router.put('/users/:id/update', function(req, res, next){
+router.put('/:id/update', function(req, res, next){
   
-// })
+})
 
-router.delete('/:id/reserve/:reservation_id', function(req, res, next){
+router.delete('/:id/remove', function(req, res, next){
   
 })
 
