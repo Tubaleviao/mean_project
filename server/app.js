ea848d46db4952adb893a39fc22e4af254bc5c42
Tubaleviao <alvaro.prata.filho@gmail.com>
2020-02-01 14:55:10 -0600

    added database connection

--- server/app.js
+++ server/app.js
@@ -3,12 +3,14 @@ var path = require('path');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const {database} = require('./middleware')
 
 var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(database)
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ server/middlewares.js
@@ -0,0 +1,26 @@
+const MongoClient = require('mongodb').MongoClient
+const {verify} = require('jsonwebtoken');
+
+const client = new MongoClient(process.env.DB_HOST, 
+    { useNewUrlParser: true, useUnifiedTopology: true })
+let db
+
+const database = async (req, res, next) => {
+    if(!db){
+        const worked = await client.connect()
+        db = client.db('homework')
+    }
+    req.db = db
+    next()
+}
+
+const auth = async (req, res, next) => {
+    try{
+        let json = verify(req.headers.token, process.env.JWT_KEY)
+        next()
+    }catch(err){
+        next({msg: "token not valid", error: err})
+    }
+}
+
+module.exports = {database, auth}
