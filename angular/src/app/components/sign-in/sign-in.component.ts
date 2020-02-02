8db849468c8cf9f2f10a00f412bc4bc0549737bc
Yadir Hernandez Batista <yadirhb@gmail.com>
2020-02-01 22:59:42 -0600

    Finishing Reactive forms in Sign-in screen.

--- angular/src/app/components/sign-in/sign-in.component.ts
+++ angular/src/app/components/sign-in/sign-in.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  selector: "app-sign-in",
+  templateUrl: "./sign-in.component.html",
+  styleUrls: ["./sign-in.component.scss"]
 })
 export class SignInComponent implements OnInit {
+  signInForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+    this.signInForm = fb.group({
+      username: ["", Validators.required],
+      password: ["", [Validators.required, Validators.minLength(8)]]
+    });
   }
 
+  get password() {
+    return this.signInForm.get("password");
+  }
+
+  get username() {
+    return this.signInForm.get("username");
+  }
+
+  ngOnInit() {}
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.signInForm.value);
+  }
 }
