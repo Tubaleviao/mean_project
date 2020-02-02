ff7d0cceeae346ec46e2258f27cada0024df3a7f
Yosief Teklemariam <yosiefnt@gmail.com>
2020-02-02 13:21:47 -0600

    middle ware added for validation, signup router added , some npm packages added like @hapi/joi and bcrypt

--- server/middlewares.js
+++ server/middlewares.js
@@ -1,9 +1,12 @@
 const MongoClient = require('mongodb').MongoClient
 const {verify} = require('jsonwebtoken');
+//VALIDATION package
+const joi = require('@hapi/joi');
 
 const client = new MongoClient(process.env.DB_HOST, 
     { useNewUrlParser: true, useUnifiedTopology: true })
-let db
+
+    let db
 
 const database = async (req, res, next) => {
     if(!db){
@@ -16,7 +19,7 @@ const database = async (req, res, next) => {
 
 const auth = async (req, res, next) => {
     try{
-        let json = verify(req.headers.token, process.env.JWT_KEY)
+        let json = verify(req.headers.token, process.env.JWT_KEY)  // suggestion.. add await
         req.userinfo = json
         next()
     }catch(err){
@@ -24,4 +27,16 @@ const auth = async (req, res, next) => {
     }
 }
 
-module.exports = {database, auth}
+//signup validation
+const signupValidation = (data) => {
+    const schema = joi.object({
+        username: joi.string().min(3).required(),
+        email: joi.string().min(4).required().email(),
+        password: joi.string().min(5).required(),
+    });
+
+  // lets validate data
+  return schema.validate(data);
+}
+
+module.exports = {database, auth, signupValidation}
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,19 +1,19 @@
 var express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt')
-const {auth} = require('../middlewares')
+const { auth, signupValidation } = require('../middlewares')
 var router = express.Router();
 
-/* GET users listing. */
+/* GET: users listing. */
 router.get('/', function(req, res, next) {
   //res.json({msg:"something"})
-  req.db.find({},  { '_id': 0, 'username': 1, 'email': 1 } )
+  req.db.collection('users').find({},  { '_id': 0, 'username': 1, 'email': 1 } )
         .toArray((err, documents) => {
             res.status(200).json(documents);
         });
 });
 
-/* GET user, find one. */
+/* GET: user, find one. */
 router.get("/:username", (req, res) => {
   const query = { username: req.params.username };
   req.db.collection('users').find(query).toArray((error, document) => {
@@ -22,20 +22,41 @@ router.get("/:username", (req, res) => {
   });
 });
 
-/* POST creating users */
-router.post('/signup', function(req, res, next){
-  var new_user = req.body;
-  req.db.insertOne( new_user, (err, user_inserted) => {
-    if(error) throw error;
-    res.end("Congratulations, user successfully added \n" + user_inserted);
-  });
+/* POST: signup users */
+router.post('/signup', async function(req, res, next){
+  // checking validation
+  const { error } = signupValidation(req.body);
+  if (error) return res.status(400).json({message: error.details[0].message});
+
+  // check if the user is already in database
+  const email_exist = await req.db.collection('users').findOne({ email: req.body.email });
+  if (email_exist) return res.status(400).json({message :"This email is already taken!"});
+
+  // hash the passwords
+  const salt = await bcrypt.genSalt(10);
+  const hashed_password = await bcrypt.hash(req.body.password, salt);
+
+  // create new user
+  const new_user = {
+    username: req.body.name,
+    email: req.body.email,
+    password: hashed_password
+  };
+
+  try {
+    const saved_user = await req.db.collection('users').insertOne(new_user);
+    res.json({ success: "The new user is added!", user: saved_user._id });
+  } catch (err) {
+      res.status({message:err.message}); // res.status({message:'Error Occured'});
+  }
 })
 
-/* POST signin users */
-router.post('/signin', function(req, res, next){
+/* POST: signin users */
+router.post('/signin', async function(req, res, next){
   
 })
 
+/* PUT: update users */
 router.put('/update/:id/', function(req, res, next){
   
 })
@@ -47,7 +68,7 @@ router.delete('/remove/:username', function(req, res, next){
 })
   
 router.get('/jwt', (req,res) => {
-  bcrypt.hash('comming_from_req', 10, (err, hash) => {
+  bcrypt.hash('password_comming_from_req', 10, (err, hash) => {
       var token = jwt.sign({ 
           username: 'user',
           password: hash
