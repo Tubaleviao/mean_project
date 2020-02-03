487ae60579a78d63b92d9639d7b31d847c2f18dd
Tubaleviao <alvaro.prata.filho@gmail.com>
2020-02-03 17:30:11 -0600

    Done

--- server/app.js
+++ server/app.js
@@ -1,13 +1,17 @@
 require('dotenv').config()
+let http = require('http');
 var express = require('express');
 var path = require('path');
 const cors = require('cors')
+const socketIO = require('socket.io')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const {database} = require('./middlewares')
 
 var app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(cors())
 app.use(express.json());
@@ -24,5 +28,16 @@ app.use(function(err, req, res, next) {
   else next();
 });
 
-module.exports = app;
+io.on('connection', socket => {
+  console.log('user connected');
+  socket.on('new-message', message => {
+    console.log(message);
+    // { lng: -91.9590777, lat: 41.013430500000005 }
+    socket.emit('new-message', message)
+    // TODO: save location at the database
+  });
+});
+
+// this works with socket
+server.listen(3000, ()=>console.log("Resting at 3000"))
 
