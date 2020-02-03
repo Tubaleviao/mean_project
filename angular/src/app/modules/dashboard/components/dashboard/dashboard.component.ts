9f2eaeefea01e8c2d8576b5812423a5940ca75e6
Yadir Hernandez Batista <yadirhb@gmail.com>
2020-02-03 11:34:52 -0600

    Added changes in the dashboard

--- angular/src/app/modules/dashboard/components/dashboard/dashboard.component.ts
+++ angular/src/app/modules/dashboard/components/dashboard/dashboard.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Renderer2,
+  Inject,
+  AfterViewInit,
+  ViewChild,
+  ElementRef
+} from "@angular/core";
+import { DOCUMENT } from "@angular/common";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.scss"]
 })
 export class DashboardComponent implements OnInit {
+  GOOGLE_API_KEY: string = "AIzaSyC1_174lsuQyXXqp83sKBuyuCnP2c0hAsQ";
+  @ViewChild("mapContainer", { static: false }) gmap: ElementRef;
+  map: google.maps.Map;
 
-  constructor() { }
+  lat = 40.73061;
+  lng = -73.935242;
+
+  // coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+  constructor(
+    private _r2: Renderer2,
+    @Inject(DOCUMENT) private _document: Document
+  ) {}
 
   ngOnInit() {
-  }
+    let script = this._r2.createElement("script");
+    //<script async defer src="https://maps.googleapis.com/maps/api/js?key={{GOOGLE_API_KEY}}&callback=initMap" type="text/javascript"></script>
+    script.type = `text/javascript`;
+    // script.async = true;
+    // script.defer = true;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${this.GOOGLE_API_KEY}`; //&callback=initMap
+    // script.text = `
+    //         {
+    //             "@context": "https://schema.org"
+    //             /* your schema.org microdata goes here */
+    //         }
+    //     `;
 
+    this._r2.appendChild(this._document.body, script);
+  }
 }
